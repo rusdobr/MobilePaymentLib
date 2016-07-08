7776e5ae8e190065706503fd8ec70eda2de8cacb
Ruslan <rusdobr@gmail.com>
2016-07-08 08:14:47 +0300

    First commit

---
+++ build/preprocessed/ua/telnumberident/ruslan/ITelephoneProviderIdent.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ua.telnumberident.ruslan;
+
+/**
+ *
+ * @author ruslan
+ */
+public interface ITelephoneProviderIdent {
+    /**
+     * 
+     * @param number
+     * @return 
+     */
+    public PhoneProvider[] ident(PhoneNumber number); 
+    /**
+     * 
+     * @param number
+     * @return 
+     */
+    public PhoneMaskItem[] search (PhoneNumber number);
+}
