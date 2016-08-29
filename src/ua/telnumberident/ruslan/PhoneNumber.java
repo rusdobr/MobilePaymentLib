8d39574aff735931e734f86bf359e359064d36e4
Ruslan <rusdobr@gmail.com>
2016-08-29 22:24:31 +0300

    NhoneNumber implemented

--- src/ua/telnumberident/ruslan/PhoneNumber.java
+++ src/ua/telnumberident/ruslan/PhoneNumber.java
@@ -9,6 +9,28 @@ package ua.telnumberident.ruslan;
  *
  * @author ruslan
  */
-public class PhoneNumber {
+public final class PhoneNumber {
+
+    private final String number;
     
+    public PhoneNumber(String number) {
+        this.number = number;        
+    }
+    
+    String value() {
+        return this.number;
+    }
+    
+    //@Override
+    public String toString() {
+        return this.value();
+    }
+    /**
+     *
+     * @param object2
+     * @return
+     */
+    public boolean equals(Object object2) {
+        return object2 instanceof PhoneNumber && this.number.equals(((PhoneNumber)object2).value());
+    }
 }
